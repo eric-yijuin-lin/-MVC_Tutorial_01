c3f32b41c0937213be4a00931c4a2dff43069713
Eric Lin <eric.yijuin.lin@gmail.com>
2021-09-16 14:53:24 +0800

    true initial commit

---
+++ MVC_Tutorial_01/Controllers/NewsController.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVC_Tutorial_01.Models;
+
+namespace MVC_Tutorial_01.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly MvcExamDBContext _context;
+
+        public NewsController(MvcExamDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: News
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.TblNews.ToListAsync());
+        }
+
+        // GET: News/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tblNews = await _context.TblNews
+                .FirstOrDefaultAsync(m => m.No == id);
+            if (tblNews == null)
+            {
+                return NotFound();
+            }
+
+            return View(tblNews);
+        }
+
+        // GET: News/Create
+        public async Task<IActionResult> Create()
+        {
+            var newsTypes = await _context.TblNewsType.ToListAsync();
+            var selectList = newsTypes.Select(x => new SelectListItem(x.TypeText, x.TypeValue));
+            ViewData["NewsTypeSelectList"] = new SelectList(selectList, "Value", "Text");
+
+            return View();
+        }
+
+        // POST: News/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("No,Type,Title,Creator,Content,CreateDt")] TblNews tblNews)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tblNews);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tblNews);
+        }
+
+        // GET: News/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tblNews = await _context.TblNews.FindAsync(id);
+            if (tblNews == null)
+            {
+                return NotFound();
+            }
+            return View(tblNews);
+        }
+
+        // POST: News/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("No,Type,Title,Creator,Content,CreateDt")] TblNews tblNews)
+        {
+            if (id != tblNews.No)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tblNews);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TblNewsExists(tblNews.No))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tblNews);
+        }
+
+        // GET: News/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tblNews = await _context.TblNews
+                .FirstOrDefaultAsync(m => m.No == id);
+            if (tblNews == null)
+            {
+                return NotFound();
+            }
+
+            return View(tblNews);
+        }
+
+        // POST: News/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tblNews = await _context.TblNews.FindAsync(id);
+            _context.TblNews.Remove(tblNews);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public void MoreLinq()
+        {
+            // first, first or default
+            var allNews = _context.TblNews.ToList();
+            try
+            {
+                var firstNews1 = allNews
+                    .First(x => x.Title == "Today");
+                var firstNews2 = allNews
+                    .First(x => x.Title == "No such title");
+            }
+            catch (Exception ex)
+            {
+                string errorMessage = ex.Message;
+                var firstNews3 = allNews
+                    .FirstOrDefault(x => x.Title == "No such title");
+            }
+
+            // where
+            var todayNews = allNews.Where(x => x.Title == "Today").ToList();
+            var todayNewsToList = allNews.Where(x => x.Title == "Today").ToList();
+
+            // orderby
+            var orderedNews1 = allNews.OrderBy(x => x.CreateDt).ToList();
+            var orderedNews2 = allNews.OrderByDescending(x => x.CreateDt).ToList();
+
+            // min, max
+            var earliestDateTime = allNews.Min(x => x.CreateDt);
+            var latestDateTime = allNews.Max(x => x.CreateDt);
+
+            // count, sum
+            var newsCount1 = allNews.Count();
+            var newsCount2 = allNews.Count(x => x.Title == "Today");
+            var sum1 = allNews.Sum(x => x.No);
+            var sum2 = allNews.Where(x => x.No > 5).Sum(x => x.No);
+
+            // select
+            var select1 = allNews.Select(x => x.Title).ToList();
+            var select2 = allNews.Select(x => new { x.Title, x.CreateDt }).ToList();
+            var select3 = allNews.Select(x => 
+                new TestClass() 
+                {
+                    TestTitle = x.Title,
+                    TestDt = x.CreateDt 
+                }
+            ).ToList();
+
+            // distinct
+            var distinctCreator = allNews
+                .Select(x => x.Creator)
+                .Distinct()
+                .ToList();
+        }
+
+        private bool TblNewsExists(int id)
+        {
+            return _context.TblNews.Any(e => e.No == id);
+        }
+    }
+
+    public class TestClass
+    {
+        public string TestTitle { get; set; }
+        public DateTime? TestDt { get; set; }
+    }
+}
---
+++ MVC_Tutorial_01/Models/MvcExamDBContext.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace MVC_Tutorial_01.Models
+{
+    public partial class MvcExamDBContext : DbContext
+    {
+        public MvcExamDBContext()
+        {
+        }
+
+        public MvcExamDBContext(DbContextOptions<MvcExamDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblCustomer> TblCustomer { get; set; }
+        public virtual DbSet<TblNews> TblNews { get; set; }
+        public virtual DbSet<TblNewsType> TblNewsType { get; set; }
+        public virtual DbSet<TblUser> TblUser { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=LAPTOP-8RA02U0H;Initial Catalog=MvcExamDB;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TblCustomer>(entity =>
+            {
+                entity.HasKey(e => e.CCustomerId);
+
+                entity.Property(e => e.CCustomerId).HasColumnName("cCustomerID");
+
+                entity.Property(e => e.CCreatorDt)
+                    .HasColumnName("cCreatorDT")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.CCreatorId).HasColumnName("cCreatorID");
+
+                entity.Property(e => e.CCustomerAddress)
+                    .IsRequired()
+                    .HasColumnName("cCustomerAddress")
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.CCustomerName)
+                    .IsRequired()
+                    .HasColumnName("cCustomerName")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.CCustomerPhone)
+                    .IsRequired()
+                    .HasColumnName("cCustomerPhone")
+                    .HasMaxLength(200);
+            });
+
+            modelBuilder.Entity<TblNews>(entity =>
+            {
+                entity.HasKey(e => e.No);
+
+                entity.Property(e => e.Content)
+                    .IsRequired()
+                    .HasColumnType("text");
+
+                entity.Property(e => e.CreateDt)
+                    .HasColumnName("CreateDT")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Creator).HasMaxLength(50);
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.Type)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TblNewsType>(entity =>
+            {
+                entity.HasKey(e => e.TypeId);
+
+                entity.Property(e => e.TypeText)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.TypeValue)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TblUser>(entity =>
+            {
+                entity.HasKey(e => e.CUserId);
+
+                entity.Property(e => e.CUserId).HasColumnName("cUserID");
+
+                entity.Property(e => e.CCreatorDt)
+                    .HasColumnName("cCreatorDT")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.CCreatorId).HasColumnName("cCreatorID");
+
+                entity.Property(e => e.CUserAccout)
+                    .IsRequired()
+                    .HasColumnName("cUserAccout")
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.CUserMail)
+                    .IsRequired()
+                    .HasColumnName("cUserMail")
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.CUserName)
+                    .IsRequired()
+                    .HasColumnName("cUserName")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.CUserPassword)
+                    .IsRequired()
+                    .HasColumnName("cUserPassword")
+                    .HasMaxLength(20);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
